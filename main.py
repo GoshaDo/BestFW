df0ef0768734aa58a4fc66218d1e1a8685c3328b
george.dozoretz <goshatoo@gmail.com>
2021-10-06 20:15:41 +0300

    Gosha

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from api import API
 
 app = Flask(__name__)
-
+loc = ""
 
 @app.route('/')
 def index():
@@ -20,6 +20,18 @@ def search_loc(location):
     api = API()
     location_list = api.get_loc(location)
     return render_template("choose_location.html", loc_list=location_list)
+
+
+@app.route('/loc/<location>', methods=['POST'])
+def choose_loc(location):
+    text = "chosen location"
+    return redirect(url_for('weather', location=location))
+
+
+@app.route('/weather/<location>')
+def weather(location):
+    text = f"weather in {location}"
+    return text
 
 
 if __name__ == '__main__':
