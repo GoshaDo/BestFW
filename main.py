afe273d16ca56580c3e19ce7983357e360e1992c
george.dozoretz <goshatoo@gmail.com>
2021-10-06 16:02:02 +0300

    Gosha first file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main_page():
-    return "Best Freaking Whether"
+    if request.method == 'GET':
+        return "render_template('home_page.html')"
+    if request.method == 'POST':
+        user_input = request.form
+        return f"{user_input}"
 
 
-@app.route("/<city>_<country>")
+@app.route("/<city>_<country>", methods=['GET'])
 def forecast_page(city, country):
     return f"whether in {city}, {country}"
