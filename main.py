6fa636b2b5f93b2e9ab310b098644151af87bd18
george.dozoretz <goshatoo@gmail.com>
2021-10-06 17:23:14 +0300

    Gosha first file

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def main_page():
-    return render_template('home_page.html')
+class UserInputForm(FlaskForm):
+    user_input = StringField('City or Country',
+                             validators=[DataRequired(), Length(min = 2, max = 25)])
+    submit = SubmitField("GO!")
 
 
-@app.route("/", methods = ['POST'])
-def data_insertation():
-    user_input = request.form['input_text']
-    return user_input
+@app.route("/home")
+def home():
+    form = UserInputForm()
+    return render_template("home.html", title='Home Page', form=form)
+
 
 @app.route("/<city>_<country>", methods=['GET'])
 def forecast_page(city, country):
     return f"whether in {city}, {country}"
 
 
- if __name__ == "__main__":
-     # Launch the Flask dev server
-     app.run(host="localhost", debug=True)
+if __name__ == "__main__":
+    # Launch the Flask dev server
+    app.run(host="localhost", debug=False)
