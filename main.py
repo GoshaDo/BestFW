0357b5e35754b7f608a86731fe373d8ecabd8938
george.dozoretz <goshatoo@gmail.com>
2021-10-06 16:11:38 +0300

    Gosha first file

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def main_page():
-    if request.method == 'GET':
-        return "render_template('home_page.html')"
-    if request.method == 'POST':
-        user_input = request.form
-        return f"{user_input}"
+    return render_template('home_page.html')
 
+
+@app.route("/", methods = ['POST'])
+def data_insertation():
+    user_input = request.form['input_text']
+    return user_input
 
 @app.route("/<city>_<country>", methods=['GET'])
 def forecast_page(city, country):
