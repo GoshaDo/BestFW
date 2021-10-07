91ab3628fcf2597d67b1fefcd9ed676b6d6c5625
george.dozoretz <goshatoo@gmail.com>
2021-10-07 10:52:42 +0300

    minors

--- utilities.py
+++ utilities.py
@@ -37,20 +37,3 @@ def loc_list_to_human(loc_list):
         human_loc.append(append_string)
     return human_loc
 
-
-class weather:
-    """Weather object:
-    country : string
-    city : string
-    district: string
-    max_temp : {0: int, 1: int, 2: int, 3: int, 4: int, 5: int , 6: int}
-    min_temp : {0: int, 1: int, 2: int, 3: int, 4: int, 5: int , 6: int}
-    humidity :  {0: int, 1: int, 2: int, 3: int, 4: int, 5: int , 6: int}"""
-    def __init__(self, country, city, district, max_temp, min_temp, humidity, status):
-        self.country = country
-        self.district = district
-        self.city = city
-        self.max_temp = max_temp
-        self.min_temp = min_temp
-        self.humidity = humidity
-        self.status = status
