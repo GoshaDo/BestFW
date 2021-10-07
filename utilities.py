12437fec56bc6dcff55eac143d7a2c47f1ac5ae2
george.dozoretz <goshatoo@gmail.com>
2021-10-07 07:09:51 +0300

    Country to capital Country code to country Limit of max input by user

--- utilities.py
+++ utilities.py
@@ -1,3 +1,21 @@
+import json
+
+
+def get_capital(state):
+    with open('database/country_capital.json') as json_file:
+        data = json.load(json_file)
+        for line in data:
+            if state.lower() == line['country'].lower():
+                return line['city']
+
+
+def rename_country(code):
+    with open('database/country_code.json') as json_file:
+        data = json.load(json_file)
+        for country in data:
+            if country['code'] == code:
+                return country['name']
+    return code
 
 def loc_list_to_human(loc_list):
     """[['Jerusalem', 'US', 'RI', 'Washington County', {'lat': 41.376701, 'lng': -71.518097}]]
@@ -6,6 +24,10 @@ def loc_list_to_human(loc_list):
     for loc in loc_list:
         append_string = ""
         append_order = [0, 3, 2, 1]
+        if len(loc[1]) < 3:
+            loc[1] = rename_country(loc[1])
+        if len(loc[0]) < 2:
+            loc[0] = get_capital(loc[1])
         for i in append_order:
             if len(loc[i]) > 0:
                 append_string += (loc[i] + " ")
